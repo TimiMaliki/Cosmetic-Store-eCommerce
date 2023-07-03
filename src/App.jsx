d678c1ff63408e01bf4a870f817cc7b5f3798d48
TimiMaliki <malikitimi@gmail.com>
2023-07-03 20:47:53 +0100

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import {FaHeart} from  'react-icons/fa'
 // import {FaSearch} from  'react-icons/fa'
 // import {FaShoppingCart} from  'react-icons/fa'
+import { Route, Routes } from 'react-router-dom'
 import index from './assets/styles/index.module.scss'
 import Header from './components/Header'
 import LogoComponent from './components/LogoComponent'
@@ -10,52 +11,67 @@ import AboutComponent from './components/AboutComponent'
 import LatestItemsComponent from './components/LatestItemsComponent'
 import CarouselComponent from './components/CarouselComponent'
 import InstagramComponent from './components/InstagramComponent'
-import  FooterComponent from './components/FooterComponent'
+import FooterComponent from './components/FooterComponent'
+import LoginPage from './components/LoginPage'
 
-
+ 
 function App() {
   return (
 
     <div className={index.navBar}>
+
       <div className={index.navBarWrapper}>
 
-      <div className={index.brandName}>
-         <h1>Maliki</h1>
-      </div>
+        <div className={index.brandName}>
+          <h1>Maliki</h1>
+        </div>
 
-      <div className={index.icons}>
-        <div className={index.iconWrapper}>
+        <div className={index.icons}>
+          <div className={index.iconWrapper}>
             <button>
               <p>Log In</p>
             </button>
+          </div>
         </div>
-      </div>
 
       </div>
-      
+
 
       <div className={index.nav}>
         <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Shop</a></li>
-          <li><a href="">Blog</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Contact</a></li>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Contact</li>
         </ul>
-      </div>
+      </div> 
+
 
 
       <Header />
-      <LogoComponent />
-      <ShopComponent />
+      <LogoComponent /> 
+       <ShopComponent />
       <PopularProductComponent />
       <AboutComponent />
       <LatestItemsComponent/>
       <CarouselComponent />
       <InstagramComponent />
-      <FooterComponent/>
-    </div>
+      <FooterComponent/> 
+     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+      // <LoginPage /> 
+     
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,18 @@
+import loginPage from '../assets/styles/LoginPage.module.scss'
+
+const LoginPage = () => {
+    return (
+        <div className={loginPage.login}>
+            <div className={loginPage.container}>
+                <div className={loginPage.img}>
+
+                </div>
+                <div className={loginPage.loginDetails}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
