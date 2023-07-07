64a6b3029c82bd007fcb7cccfea0f324c909d02e
TimiMaliki <malikitimi@gmail.com>
2023-07-07 21:50:56 +0100

    adjustments

--- src/components/LogoComponent.jsx
+++ src/components/LogoComponent.jsx
@@ -1,18 +1,20 @@
 import logo from '../assets/styles/logo.module.scss'
 import logoImgOne from '../assets/img/LogoImg/839942_preview.jpg'
 import logoImgTwo from '../assets/img/LogoImg/18764028_rm370-08.jpg'
-import logoImgThree from '../assets/img/LogoImg/28560857_christmas_2012_new_5891.jpg'
+import a from '../assets/img/LogoImg/a.jpg'
+import r from '../assets/img/LogoImg/r.jpg'
+import g from '../assets/img/LogoImg/g.jpg'
 
 
 
 const LogoComponent = () => {
   return (
     <div className={logo.logo}>
-     <img src={logoImgOne} alt="" />
+     <img src={a} alt="" />
      <img src={logoImgTwo} alt="" />
      <img src={logoImgOne} alt="" />
-     <img src={logoImgOne} alt="" />
-     <img src={logoImgThree} alt="" />
+     <img src={r} alt="" />
+     <img src={g} alt="" />
     </div>
   )
 }
